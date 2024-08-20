0c1a7891a43f57ba0b65940d7aa6957ff106cdef
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-08-20 13:29:00 -0500

    Styles to layout component

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, UpperCasePipe],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
